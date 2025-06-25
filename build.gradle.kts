649dbe73eb4a0e5faf1f92763df9bf7974d48d50
RECHERGG <95080877+RECHERGG@users.noreply.github.com>
2025-06-25 15:24:51 +0200

    inital commit with logging

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    java
+}
+
+group = "de.rechergg"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    annotationProcessor(libs.bundles.utils)
+    implementation(libs.bundles.logging)
+    implementation(libs.bundles.utils)
+    implementation(libs.okhttp)
+    implementation(libs.gson)
+    implementation(libs.databind)
+
+    testAnnotationProcessor(libs.bundles.utils)
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
