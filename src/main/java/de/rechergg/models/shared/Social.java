bcae924f247635803cae8f2b8f563b7904af46b2
RECHERGG <95080877+RECHERGG@users.noreply.github.com>
2025-07-02 15:50:09 +0200

    added Social Model

---
+++ src/main/java/de/rechergg/models/shared/Social.java
@@ -0,0 +1,17 @@
+package de.rechergg.models.shared;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Value;
+import lombok.extern.jackson.Jacksonized;
+
+@Value
+@Builder
+@Jacksonized
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Social {
+
+    String platform;
+
+    String url;
+}
