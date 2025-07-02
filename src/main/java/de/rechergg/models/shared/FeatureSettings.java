cc14e4a62373fc622e1b848d5fcc7094166d2707
RECHERGG <95080877+RECHERGG@users.noreply.github.com>
2025-07-02 15:50:53 +0200

    added FeatureSettings Model

---
+++ src/main/java/de/rechergg/models/shared/FeatureSettings.java
@@ -0,0 +1,17 @@
+package de.rechergg.models.shared;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Value;
+import lombok.extern.jackson.Jacksonized;
+
+@Value
+@Builder
+@Jacksonized
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class FeatureSettings {
+
+    @JsonProperty("issue_funding_enabled")
+    Boolean issueFundingEnabled;
+}
