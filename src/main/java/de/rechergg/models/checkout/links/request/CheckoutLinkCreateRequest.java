39cfcdea279d4671db666c3a3f2a568968b3146b
RECHERGG <95080877+RECHERGG@users.noreply.github.com>
2025-07-04 15:01:54 +0200

    added new Checkout Link Create Request Model

---
+++ src/main/java/de/rechergg/models/checkout/links/request/CheckoutLinkCreateRequest.java
@@ -0,0 +1,51 @@
+package de.rechergg.models.checkout.links.request;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Value;
+import lombok.extern.jackson.Jacksonized;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Model for creating a Checkout Link via Polar API.
+ * https://docs.polar.sh/api-reference/checkout-links/create
+ */
+@Value
+@Builder
+@Jacksonized
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class CheckoutLinkCreateRequest {
+
+    @JsonProperty("payment_processor")
+    String paymentProcessor;
+
+    @JsonProperty("product_price_id")
+    String productPriceId;
+
+    @JsonProperty("product_id")
+    String productId;
+
+    @JsonProperty("products")
+    List<String> products;
+
+    @JsonProperty("metadata")
+    Map<String, String> metadata;
+
+    String label;
+
+    @JsonProperty("allow_discount_codes")
+    Boolean allowDiscountCodes;
+
+    @JsonProperty("require_billing_address")
+    Boolean requireBillingAddress;
+
+    @JsonProperty("discount_id")
+    String discountId;
+
+    @JsonProperty("success_url")
+    String successUrl;
+}
